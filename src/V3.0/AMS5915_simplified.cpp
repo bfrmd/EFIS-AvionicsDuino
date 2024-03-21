0a3694ca96f60fa53f29e92112edad2e442ccf90
bfrmd <58253078+bfrmd@users.noreply.github.com>
2024-03-21 18:10:42 +0100

    Add files via upload

---
+++ src/V3.0/AMS5915_simplified.cpp
@@ -0,0 +1,85 @@
+#include "AMS5915_simplified.h"
+
+AMS5915_simplified::AMS5915_simplified(TwoWire &bus, uint8_t address, Transducer type) // constructor, I2C bus, sensor address, and transducer type
+{
+  _bus = &bus;           // I2C bus
+  _address = address;    // I2C address
+  _type = type;          // transducer type
+}
+
+int AMS5915_simplified::begin() // starts the I2C communication and sets the pressure and temperature ranges using getTransducer
+{
+  _bus->begin();                              // starting the I2C bus
+  _bus->setClock(_i2cRate);                   // setting the I2C clock
+  getTransducer();                            // setting the min and max pressure based on the chip
+  for (size_t i = 0; i < _maxAttempts; i++)   // checking to see if we can talk with the sensor
+  {
+    _status = readBytes(&_pressureCounts, &_temperatureCounts);
+    if (_status > 0) {
+      break;
+    }
+    delay(10);
+  }
+  return _status;
+}
+
+int AMS5915_simplified::readSensor(char letter)  // reads data from the sensor
+{
+  uint16_t digOutPmin;
+  switch (letter)
+  {
+    case 'A' :  digOutPmin = 1638; break;
+    case 'D' : digOutPmin = 1569; break; // le capteur différentiel doit avoir un problème d'étalonnage en usine, la valeur pour une pression différentielle nulle devrait être 1638, hors elle est à 1569
+  }
+
+  _status = readBytes(&_pressureCounts, &_temperatureCounts);                                                                                                 // get pressure and temperature counts off transducer
+  _data.Pressure_Pa = (((float)(_pressureCounts - digOutPmin)) / (((float)(_digOutPmax - digOutPmin)) / ((float)(_pMax - _pMin))) + (float)_pMin) * _mBar2Pa; // convert counts to pressure, PA
+  _data.Temp_C = (float)((_temperatureCounts * 200)) / 2048.0f - 50.0f;                                                                                       // convert counts to temperature, C
+  return _status;
+}
+
+float AMS5915_simplified::getPressure_Pa() // returns the pressure value, PA
+{
+  return _data.Pressure_Pa;
+}
+
+float AMS5915_simplified::getTemperature_C()  // returns the temperature value, C
+{
+  return _data.Temp_C;
+}
+
+void AMS5915_simplified::getTransducer()  // sets the pressure range based on the chip
+{
+  switch (_type) // setting the min and max pressures based on which transducer it is
+  {
+    case AMS5915_0050_D:
+      _pMin = AMS5915_0050_D_P_MIN;
+      _pMax = AMS5915_0050_D_P_MAX;
+      break;
+
+    case AMS5915_1500_A:
+      _pMin = AMS5915_1500_A_P_MIN;
+      _pMax = AMS5915_1500_A_P_MAX;
+      break;
+  }
+}
+
+int AMS5915_simplified::readBytes(uint16_t* pressureCounts, uint16_t* temperatureCounts) // reads pressure and temperature and returns values in counts
+{
+  _numBytes = _bus->requestFrom(_address, sizeof(_buffer)); // read from sensor
+  if (_numBytes == sizeof(_buffer)) // put the data in buffer
+  {
+    _buffer[0] = _bus->read();
+    _buffer[1] = _bus->read();
+    _buffer[2] = _bus->read();
+    _buffer[3] = _bus->read();
+    *pressureCounts = (((uint16_t) (_buffer[0] & 0x3F)) << 8) + (((uint16_t) _buffer[1])); // assemble into a uint16_t
+    *temperatureCounts = (((uint16_t) (_buffer[2])) << 3) + (((uint16_t) _buffer[3] & 0xE0) >> 5);
+    _status = 1;
+  }
+  else
+  {
+    _status = -1;
+  }
+  return _status;
+}
---
+++ src/V3.0/HorizArt.h
@@ -0,0 +1,104 @@
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//                             Bibliothèque HorizArt : création d'un horizon artificiel complet,
+//        prêt à être intégré dans un programme d'EFIS utilisant un écran basé sur un contrôleur RA8875
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+/*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  //                                                 HorizArt.h
+  //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+  Copyright (c) 2022 AvionicsDuino - benjamin.fremond@avionicsduino.com
+  Contributors : Benjamin Frémond - Gabriel Consigny - Michel Riazuelo
+
+    HorizArt.h is part of EFIS_Avionicsduino_V2.1
+
+    EFIS_Avionicsduino_V2 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************************************************************/
+#ifndef HORIZART_H
+#define HORIZART_H
+
+#include <SPI.h>
+#include <RA8875.h>
+#include "HorizArtParamGen.h"
+
+class HorizArt
+{
+    //  -------------------------------------------------------------------- Attributs de la classe --------------------------------------------------------------------------------------------
+  private:
+
+    RA8875 * ptr_tft = 0;
+
+    // Variables définissant la taille et la position du cadre
+    int LD, LG, LH, LB;
+    int largeur, hauteur;
+    int Ybille;
+
+    // Variables utilisées pour tracer l'arc fixe de l'indicateur d'angle d'inclinaison et ses graduations 
+    const int AngleGraduationCourte[6] = {315, 340, 350, 10, 20, 45};
+    const int AngleGraduationLongue[5] = {300, 330, 360, 30, 60};
+    byte RayonGraduations;
+    byte racpiai;
+
+    // Variables utilisées pour tracer les graduations mobiles du pitch ****** les constantes gagneront à devenir des variables initialisées (en fonction de la taille de l'écran) dans le constructeur
+    float PasGrad;
+    float demilonggrad;
+    float k;
+
+    // variables "de service" servant à "enregistrer" une position quelconque de "curseur graphique" en vue d'un tracé (ou d'un effacement) de ligne (ou autre) à partir de ce point.
+    int curseurGx1, curseurGy1;
+    int curseurGx2, curseurGy2;
+    int curseurGx3, curseurGy3;
+    int curseurGxold1, curseurGyold1;
+    int curseurGxold2, curseurGyold2;
+    int curseurGxold3 = 7;
+    int curseurGyold3 = 7;
+    int Mx, My;
+    int Mxold, Myold;
+
+    //  Variables utilisées pour le calcul de la position puis l'affichage de l'horizon
+    int g;
+    int d;
+    int h;
+    int b;
+    byte casG, casD;
+
+    // Variables relatives à la position de l'avion et à la symétrie du vol
+    float roll = 0.0;
+    float rollold = 0.0;
+    float pitch = 0.0;
+    float sinroll, cosroll, tanroll;
+
+    int16_t offsetBille = 0;
+    int16_t offsetBilleOld;
+
+    // Variables diverses 
+    uint16_t  BLUESKY;
+    uint16_t  OCHRE;
+
+    //  --------------------------------------------------------------------- Méthodes de la classe ---------------------------------------------------------------------------------------------------------------
+
+    void fillPentagone(int x0, int y0, int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4, uint16_t color);
+    void fillQuadrilatere (int x0, int y0, int x1, int y1, int x2, int y2, int x3, int y3, uint16_t color);
+    void triangleIso (int xS, int yS, int angle, int longueur, uint16_t couleur);
+    void bille(int8_t ofs, uint16_t couleur);
+
+
+  public:
+
+    HorizArt(RA8875 * ptr);
+    void begin();
+    void draw(float aRoll, float aPitch, uint8_t n, float aOffsetBille, bool menuOpen); // n en réserve pour l'avenir, inutile pour le moment.
+    void reDraw();
+};
+
+#endif
